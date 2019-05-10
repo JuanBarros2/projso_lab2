0d9571f550ed00ba7d3467d5917d53c57d59a173
José Renan <jose.renan@ext.tfgco.com>
2019-05-10 17:36:27 -0300

    Corrige problemas no plot

--- lab4/aging.py
+++ lab4/aging.py
@@ -4,35 +4,28 @@ class Aging:
   """How many bits to use for the Aging algorithm"""
 
   def __init__(self):
-    self.pages = []
+    self.pages = {}
 
   def put(self, frameId):
     """Allocates this frameId for some page"""
-    self.pages.append([frameId, self.ALGORITHM_AGING_NBITS * '0', 0])
+    self.pages[frameId] = self.ALGORITHM_AGING_NBITS * '0'
 
   def evict(self):
     """Deallocates a frame from the physical memory and returns its frameId"""
     min_count = 256
-    min_idx = -1
-    for idx, page in enumerate(self.pages):
-      aux = int(page[1], 2)
-      if aux < min:
+    for frameId in self.pages:
+      page = self.pages[frameId]
+      aux = int(page, 2)
+      if aux < min_count:
         min_count = aux
-        min_idx = idx
-    return self.pages.pop(min_idx)[0]
-
-  def shift(self, page):
-    bits = str(page[2])
-    for i in range(1, 7):
-      bits += page[1][i]
-    page[1] = bits
-    page[2] = 0
+        min_frameId = frameId
+    self.pages.pop(min_frameId, None)
+    return min_frameId
 
   def clock(self):
     """The amount of time we set for the clock has passed, so this is called"""
-    for page in self.pages:
-      self.shift(page)
+    for frameId in self.pages:
+      self.pages[frameId] = '0' + self.pages[frameId][0:7]
 
   def access(self, frameId, isWrite):
-    for page in self.pages:
-      if page[0] == frameId: page[2] = 1
+      self.pages[frameId] = '1' + self.pages[frameId][0:7]
